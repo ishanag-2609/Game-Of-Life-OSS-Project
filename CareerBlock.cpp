1dc208f5bd048fa71319c45c761aa8c240d00c43
Avneesh Kumar Pathak <2501030289@mail.jiit.ac.in>
2026-06-29 22:40:48 +0530

    feat(milestone): map job roles and corresponding salary data matrices

---
+++ CareerBlock.cpp
@@ -0,0 +1,7 @@
+// CareerBlock.cpp
+#include "CareerBlock.h"
+#include <iostream>
+const std::string CareerBlock::jobs[4] = {
+    "Software Engineer", "Data Analyst", "Medical Doctor", "Creative Artist"
+};
+const long CareerBlock::salaries[4] = {7000, 6000, 12000, 4500};
