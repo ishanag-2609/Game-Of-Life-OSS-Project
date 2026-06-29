87feccceb7ea3d45da32f5d4875fff3a829bfffa
Avneesh Kumar Pathak <2501030289@mail.jiit.ac.in>
2026-06-29 23:22:15 +0530

    feat(property): initialize property listing options and valuations

---
+++ HouseBlock.cpp
@@ -0,0 +1,6 @@
+// HouseBlock.cpp
+#include "HouseBlock.h"
+#include <iostream>
+const std::string HouseBlock::houses[3] = {"Suburban Condo", "Luxury Apartment", 
+"Metropolitan Mansion"};
+const long HouseBlock::costs[3] = {40000, 75000, 150000};
