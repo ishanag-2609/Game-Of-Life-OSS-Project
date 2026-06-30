ae2fbe23415e910298cb4a0448c396dab6cb3128
Kartik Singhal <2501030278@mail.jiit.ac.in>
2026-06-30 12:48:27 +0530

    style(map): balance indentation alignments and sanitize comments

--- GameMap.cpp
+++ GameMap.cpp
@@ -15,12 +15,14 @@ board[100].description = "The Retirement Gate!";
 }
 
 void GameMap::generateMapLayout() {
+
 // Distribute Career Blocks at fixed spaces (e.g., 5, 25, 55)
 int careerSpots[] = {5, 25, 55};
 for (int pos : careerSpots) {
 board[pos].type = CAREER;
 board[pos].description = "Career Milestone! Choose or upgrade occupations.";
 }
+
 // Distribute Real Estate House Blocks (e.g., 15, 45, 75)
 int houseSpots[] = {15, 45, 75};
 for (int pos : houseSpots) {
@@ -33,12 +35,14 @@ for (int i = 10; i < 100; i += 10) {
 board[i].type = PAYDAY;
 board[i].description = "Payday! Collect your standard salary revenue.";
 }
+
 // Gamble spots (e.g., 35, 65, 85)
 int gambleSpots[] = {35, 65, 85};
 for (int pos : gambleSpots) {
 board[pos].type = GAMBLE;
 board[pos].description = "Casino Zone! Wager assets on probability rules.";
 }
+
 // Assign generic random life events to any remaining spots ending in 3 or 7
 for (int i = 1; i < 100; i++) {
 if (board[i].type == EMPTY && (i % 10 == 3 || i % 10 == 7)) {
@@ -47,6 +51,7 @@ board[i].description = "Dynamic Incident! Prepare for windfalls or
 emergencies.";
 }
 }
+
 board[35].description = "The Grand Royale Casino Lounge.";
 board[55].description = "Mid-Life Career Crisis Point! Upgrade parameters.";
 board[85].description = "High-Stakes Las Vegas Underground Wagers.";
